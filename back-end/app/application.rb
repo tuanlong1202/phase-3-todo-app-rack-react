536a6348aa7df295f69692faea17d5fe22e6435f
tuanlong1202 <tuanlong1202@gmail.com>
2024-08-23 19:17:48 -0400

    Upload, ready for next steps.

--- back-end/app/application.rb
+++ back-end/app/application.rb
@@ -7,16 +7,84 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/test/) 
+    # root url
+    if req.path == '/'
+      return [
+        200,
+        { 'content-type' => 'application/json' },
+        [{ message: "you're on the home page" }.to_json]
+      ]
 
-      return [200, { 'Content-Type' => 'application/json' }, [ {:message => "response success"}.to_json ]]
-
-    else
-      resp.write "Path Not Found"
-
+    # get all resources 
+    else 
+      if req.path.match('/tasks')
+        if (req.get?)
+          task_id = req.path.split("/tasks/").last
+          if (task_id.to_i > 0)
+            t = Task.find_by(id:task_id)
+            return  [200, { 'content-type'=> 'application/json'}, [t.to_json]]
+          else 
+            return  [200, { 'content-type'=> 'application/json'}, [Task.all.to_json]]
+          end
+        elsif (req.post?)
+          t = Task.new()
+          t.create_with_attributes(to_hash(req.body.read.to_s))
+          return  [200, { 'content-type'=> 'application/json'}, [t.to_json]]
+        elsif ((req.patch?) || (req.put?))
+          task_id = req.path.split("/tasks/").last
+          if (task_id.to_i > 0)
+            t = Task.find_with_id(task_id)
+            t.update_with_attributes(to_hash(req.body.read.to_s))
+            return  [200, { 'content-type'=> 'application/json'}, [t.to_json]]
+          end
+        elsif (req.delete?)
+          task_id = req.path.split("/tasks/").last
+          if (task_id.to_i > 0)
+            t = Task.delete_by_id(task_id)
+            return  [200, { 'content-type'=> 'application/json'}, ["delete " + task_id]]
+          end
+        end
+      elsif req.path.match('/categories')
+        if (req.get?) 
+          category_id = req.path.split("/categories/").last
+          if (category_id.to_i > 0) 
+            return  [200, { 'content-type'=> 'application/json'}, [category_id.to_json]]
+          else 
+            return  [200, { 'content-type'=> 'application/json'}, [Category.all.to_json]]
+          end
+        elsif (req.post?)
+          return  [200, { 'content-type'=> 'application/json'}, [req.body.read]]
+        elsif ((req.put?) || (req.patch?))
+          category_id = req.path.split("/categories/").last
+          if (category_id.to_i > 0)
+            return  [200, { 'content-type'=> 'application/json'}, [req.body.read]]
+          end
+        elsif (req.delete?)
+          category_id = req.path.split("/categories/").last
+          if (category_id.to_i > 0)
+            return  [200, { 'content-type'=> 'application/json'}, ["delete " + category_id]]
+          end
+        end
+      end
     end
 
     resp.finish
   end
 
+  def to_hash(str,arr_sep=',', key_sep=':')
+    array = str.split(arr_sep)
+    hash = {}
+
+    array.each do |e|
+      if (e.index(':'))
+        key_value = e.delete("\n,{,},',\",`").split(key_sep)
+        key_value[1].strip!
+        hash[key_value[0].delete(" ")] = key_value[1]
+      end
+    end
+
+    return hash
+  end
+  
 end
+
--- back-end/db/seeds.rb
+++ back-end/db/seeds.rb
@@ -5,9 +5,27 @@ Task.destroy_all
 puts "Seeding Categories..."
 
 # create categories
+cat1 = Category.create(name:"Food")
+cat2 = Category.create(name:"Money")
+cat3 = Category.create(name:"Code")
+cat4 = Category.create(name:"Misc")
 
-puts "Seeding tasks..."
+puts "Seeding Tasks..."
 
 # create tasks
+task1 = Task.create(text:"Buy rice")
+task2 = Task.create(text:"Cooking rice")
+cat1.tasks << task1
+cat1.tasks << task2
+task3 = Task.create(text:"Save a tenner")
+task4 = Task.create(text:"Get an ISA")
+cat2.tasks << task3
+cat2.tasks << task4
+task5 = Task.create(text:"Build a todo app")
+task6 = Task.create(text:"Build a todo API")
+cat3.tasks << task5
+cat3.tasks << task6
+task7 = Task.create(text:"Tidy house")
+cat4.tasks << task7
 
 puts "Done!"
